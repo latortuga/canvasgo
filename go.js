004f7982d8482f590a1188197282df83433f01b6
Drew Ulmer <latortuga@gmail.com>
2009-03-21 22:13:17 -0500

    Made a sweet go-board-looking grid

--- go.js
+++ go.js
@@ -1,10 +1,15 @@
 function draw() {
-   var canvas = document.getElementById("gocanvas")
-   var ctx = canvas.getContext("2d")
-   
-   ctx.fillStyle = "orange"
-   ctx.fillRect (10, 10, 55, 50)
+    var canvas = document.getElementById("gocanvas")
+    var ctx = canvas.getContext("2d")
+    
+    drawGrid(40,40,15,ctx)
+}
 
-   ctx.fillStyle = "purple"
-   ctx.fillRect(30, 30, 55, 50)
+function drawGrid(width, height, boxes, canv) {
+    for (var i = 1; i <= boxes; i++) {
+        for (var j = 1; j <= boxes; j++) {
+            canv.strokeRect(i*width, j*height, width, height)
+        }
+    }
+    
 }
